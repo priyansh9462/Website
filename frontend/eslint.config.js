4f044fe00d52359d61ce68905a0fc2f44275e2aa
Aditya Gautam <adityagautam7704@gmail.com>
2026-03-06 12:06:48 +0530

    resolve the warning

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -27,10 +27,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "react-hooks/exhaustive-deps": "off",
+      "react-refresh/only-export-components": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
